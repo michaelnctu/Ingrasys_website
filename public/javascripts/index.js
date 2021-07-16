d4089bf2f6ef0dc2f8491fac46ecf81d313806be
MIchael Huang <michaelnctu@gmail.com>
2021-07-16 13:51:27 +0800

    j

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -163,15 +163,25 @@ $(document).on("scroll", function () {
 
   $(".text-3").css("left", Math.max(1400 - 0.3 * window.scrollY) + "px");
 
-  if (screen.width <= 768) {
-    //scroll位置可以在max後面數字改變
-    $(".text-1").css("left", Math.max(1000 - 0.35 * window.scrollY) + "px");
+  if (screen.width <= 1024) {
+    // scroll位置可以在max後面數字改變
+    $(".text-1").css("left", Math.max(1500 - 0.5 * window.scrollY) + "px");
 
-    $(".text-2").css("right", Math.max(1500 - 0.32 * window.scrollY) + "px");
+    $(".text-2").css("right", Math.max(1800 - 0.5 * window.scrollY) + "px");
 
 
-    $(".text-3").css("left", Math.max(1200 - 0.3 * window.scrollY) + "px");
+    $(".text-3").css("left", Math.max(1500 - 0.5 * window.scrollY) + "px");
   }
+
+  // if (screen.width <= 768) {
+  //   //scroll位置可以在max後面數字改變
+  //   $(".text-1").css("left", Math.max(1000 - 0.35 * window.scrollY) + "px");
+
+  //   $(".text-2").css("right", Math.max(1500 - 0.32 * window.scrollY) + "px");
+
+
+  //   $(".text-3").css("left", Math.max(1200 - 0.3 * window.scrollY) + "px");
+  // }
 
   // for iphone 11
   if (screen.width <= 420) {
@@ -182,8 +192,6 @@ $(document).on("scroll", function () {
 
     $(".text-3").css("left", Math.max(1100 - 0.3 * window.scrollY) + "px");
   }
-
-
 
   if (screen.width <= 400) {
     //scroll位置可以在max後面數字改變
